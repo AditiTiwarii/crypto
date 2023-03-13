3e58d5485ba17d6d286725c0ae3bc1a835db50c8
adititiwariiiii <tripathi.aditi9990@gmail.com>
2023-03-13 18:22:28 +0530

    initial commit

---
+++ src/components/CryptoDetails.js
@@ -0,0 +1,295 @@
+import React, { useContext, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { CryptoContext } from "../context/CryptoContext";
+
+const HighLowIndicator = ({ currentPrice, high, low }) => {
+  const [green, setGreen] = useState();
+
+  useEffect(() => {
+    let total = high - low;
+    let greenZone = ((high - currentPrice) * 100) / total;
+    setGreen(Math.ceil(greenZone));
+  }, [currentPrice, high, low]);
+
+  return (
+    <>
+      <span
+        className="bg-red h-1.5 rounded-l-lg w-[50%]"
+        style={{ width: `${100 - green}%` }}
+      >
+        &nbsp;
+      </span>
+      <span
+        className="bg-green h-1.5 rounded-r-lg w-[50%]"
+        style={{ width: `${green}%` }}
+      >
+        &nbsp;
+      </span>
+    </>
+  );
+};
+
+const CryptoDetails = () => {
+  let { coinId } = useParams();
+  let navigate = useNavigate();
+  let { getCoinData, coinData: data, currency } = useContext(CryptoContext);
+
+  useLayoutEffect(() => {
+    getCoinData(coinId);
+  }, [coinId]);
+
+  const close = () => {
+    navigate("..");
+  };
+
+  return ReactDOM.createPortal(
+    <div
+      className="fixed top-0 w-full h-full bg-blue bg-opacity-50 first-letter:
+    backdrop-blur-sm flex items-center justify-center font-nunito
+    "
+      onClick={close}
+    >
+      <div
+        className="w-[65%] h-[75%] bg-slate-300 bg-opacity-50 rounded-lg text-white relative"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {data ? (
+          <div className="flex items-center justify-between h-full w-full p-4">
+            <div className="flex flex-col w-[45%] h-full pr-2 ">
+              <div className="flex w-full items-center">
+                <img
+                  className="w-[3rem] h-[3rem] mx-1.5"
+                  src={data.image.large}
+                  alt={data.id}
+                />
+                <h1 className="text-xl capitalize font-medium">{data.name}</h1>
+                <span
+                  className="text-sm
+        py-0.5 px-2.5 ml-2 bg-cyan text-cyan bg-opacity-25
+        rounded uppercase
+        "
+                >
+                  {data.symbol}
+                </span>
+              </div>
+
+              <div className="flex w-full mt-6">
+                <div className="flex flex-col w-full">
+                  <div className="flex justify-between">
+                    <span className="text-sm capitalize text-gray-100">
+                      Price
+                    </span>
+                    <div
+                      className={`text-sm px-1 ml-2 font-medium flex items-center
+          rounded uppercase bg-opacity-25
+          ${
+            data.market_data.price_change_percentage_24h > 0
+              ? "bg-green text-green"
+              : "bg-red text-red"
+          }
+          `}
+                    >
+                      <span>
+                        {Number(
+                          data.market_data.price_change_percentage_24h
+                        ).toFixed(2)}
+                        %
+                      </span>
+                      <svg
+                        width="14"
+                        height="14"
+                        viewBox="0 0 14 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`
+                        w-[1rem] ml-0.5
+                        ${
+                          data.market_data.price_change_percentage_24h > 0
+                            ? "fill-green rotate-180"
+                            : "fill-red"
+                        }
+                        `}
+                      >
+                        <path d="M7.47951 11.4153C7.42599 11.493 7.35438 11.5565 7.27085 11.6004C7.18732 11.6444 7.09437 11.6673 7.00001 11.6673C6.90564 11.6673 6.81269 11.6444 6.72916 11.6004C6.64563 11.5565 6.57402 11.493 6.52051 11.4153L1.27051 3.83194C1.20974 3.74447 1.1741 3.64202 1.16747 3.53572C1.16084 3.42943 1.18346 3.32334 1.23289 3.229C1.28232 3.13466 1.35665 3.05567 1.44782 3.0006C1.53899 2.94554 1.6435 2.91652 1.75001 2.91669H12.25C12.3563 2.91713 12.4604 2.94652 12.5512 3.00172C12.642 3.05691 12.716 3.13581 12.7653 3.22993C12.8147 3.32406 12.8374 3.42984 12.8311 3.53591C12.8247 3.64199 12.7896 3.74433 12.7295 3.83194L7.47951 11.4153Z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <h2 className="text-lg font-bold">
+                    {new Intl.NumberFormat("en-IN", {
+                      style: "currency",
+                      currency: currency,
+                      maximumSignificantDigits: 5,
+                    }).format(data.market_data.current_price[currency])}
+                  </h2>
+                </div>
+              </div>
+
+              <div className="flex flex-col w-full mt-4 justify-between">
+                <span className="text-sm capitalize text-gray-100">
+                  total volume
+                </span>
+                <h2 className="text-base font-bold">
+                  {new Intl.NumberFormat("en-IN", {
+                    style: "currency",
+                    currency: currency,
+                    minimumFractionDigits: 0,
+                  }).format(data.market_data.total_volume[currency])}
+                </h2>
+              </div>
+
+              <div className="flex w-full mt-4 justify-between">
+                <HighLowIndicator
+                  currentPrice={data.market_data.current_price[currency]}
+                  high={data.market_data.high_24h[currency]}
+                  low={data.market_data.low_24h[currency]}
+                />
+              </div>
+
+              <div className="flex w-full mt-4 justify-between">
+                <div className="flex flex-col">
+                  <span className="text-sm capitalize text-gray-100">
+                    Low 24H
+                  </span>
+                  <h2 className="text-base font-bold">
+                    {new Intl.NumberFormat("en-IN", {
+                      style: "currency",
+                      currency: currency,
+                      minimumFractionDigits: 5,
+                    }).format(data.market_data.low_24h[currency])}
+                  </h2>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-sm capitalize text-gray-100">
+                    high 24H
+                  </span>
+                  <h2 className="text-base font-bold">
+                    {new Intl.NumberFormat("en-IN", {
+                      style: "currency",
+                      currency: currency,
+                      minimumFractionDigits: 5,
+                    }).format(data.market_data.high_24h[currency])}
+                  </h2>
+                </div>
+              </div>
+
+              <div className="flex w-full mt-4 justify-between">
+                <div className="flex flex-col">
+                  <span className="text-sm capitalize text-gray-100">
+                    max supply
+                  </span>
+                  <h2 className="text-base font-bold">
+                    {new Intl.NumberFormat("en-IN", {
+                      style: "currency",
+                      currency: currency,
+                      minimumFractionDigits: 0,
+                    }).format(data.market_data.max_supply)}
+                  </h2>
+                </div>
+                <div className="flex flex-col">
+                  <span className="text-sm capitalize text-gray-100">
+                    circulating supply
+                  </span>
+                  <h2 className="text-base font-bold">
+                    {new Intl.NumberFormat("en-IN", {
+                      style: "currency",
+                      currency: currency,
+                      minimumFractionDigits: 0,
+                    }).format(data.market_data.circulating_supply)}
+                  </h2>
+                </div>
+              </div>
+
+              <div className="flex w-full mt-4 justify-between">
+                <div className="flex flex-col">
+             
+                </div>
+
+               
+              </div>
+            </div>
+            
+
+            <div className="absolute bottom-8 right-8 flex items-center">
+              {data.links.repos_url.github[0] && (
+                <a
+                  className="text-lg px-1"
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href={data.links.repos_url.github[0]}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 24 24"
+                    style={{
+                      msTransform: "rotate(360deg)",
+                      WebkitTransform: "rotate(360deg)",
+                      transform: "rotate(360deg)",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      className="fill-cyan"
+                      fillRule="evenodd"
+                      d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z"
+                      clipRule="evenodd"
+                    />
+                    <path fill="rgba(0, 0, 0, 0)" d="M0 0h24v24H0z" />
+                  </svg>
+                </a>
+              )}
+              {data.links.twitter_screen_name && (
+                <a
+                  className="text-lg px-1"
+                  target={"_blank"}
+                  rel="noreferrer"
+                  href={`https://twitter.com/${data.links.twitter_screen_name}`}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 1024 1024"
+                    style={{
+                      msTransform: "rotate(360deg)",
+                      WebkitTransform: "rotate(360deg)",
+                      transform: "rotate(360deg)",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      className="fill-cyan"
+                      d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm215.3 337.7c.3 4.7.3 9.6.3 14.4 0 146.8-111.8 315.9-316.1 315.9-63 0-121.4-18.3-170.6-49.8 9 1 17.6 1.4 26.8 1.4 52 0 99.8-17.6 137.9-47.4-48.8-1-89.8-33-103.8-77 17.1 2.5 32.5 2.5 50.1-2a111 111 0 0 1-88.9-109v-1.4c14.7 8.3 32 13.4 50.1 14.1a111.13 111.13 0 0 1-49.5-92.4c0-20.7 5.4-39.6 15.1-56a315.28 315.28 0 0 0 229 116.1C492 353.1 548.4 292 616.2 292c32 0 60.8 13.4 81.1 35 25.1-4.7 49.1-14.1 70.5-26.7-8.3 25.7-25.7 47.4-48.8 61.1 22.4-2.4 44-8.6 64-17.3-15.1 22.2-34 41.9-55.7 57.6z"
+                    />
+                    <path fill="rgba(0, 0, 0, 0)" d="M0 0h1024v1024H0z" />
+                  </svg>
+                </a>
+              )}
+              
+            </div>
+          </div>
+        ) : (
+          <div
+            className="w-full min-h-[60vh] h-full flex justify-center items-center
+             "
+          >
+           
+            <div
+              className="w-8 h-8 border-4 border-cyan rounded-full
+             border-b-gray-200 animate-spin 
+             "
+              role="status"
+            />
+            <span className="ml-2">wait...</span>
+          </div>
+        )}
+      </div>
+    </div>,
+    document.getElementById("model")
+  );
+};
+
+export default CryptoDetails;
